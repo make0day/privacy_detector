faa24772a412eb58c5fb0a4f5f1fe8743a7c36dd
Samuel Koo <sakoo@coupang.com>
2022-01-08 22:57:10 +0900

    v3 Please enter the commit message for your changes. Lines starting

--- privacy_detector.py
+++ privacy_detector.py
@@ -14,17 +14,23 @@ from javax.swing import SwingUtilities;
 from javax.swing.table import AbstractTableModel;
 from threading import Lock
              
+import re
 from java.net import URL
+from java.util.regex import *
+from java.lang import *
+import json
+import os
 
-
-ContentTypes = [
-    "json",
-    "application/json",
-    "text/json",
-    "text/x-json",
-    "application/javascript",
-    "text/javascript",
-]
+'''
+class PII(Enum):
+    DriverLicenseNo = 1
+    BankAccountNo = 2
+    PassportNo = 3
+    IpAddress = 4
+    HomeAddress = 5
+    MobileNo = 6
+    PhoneNo = 7
+'''
 
 class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController, AbstractTableModel):
     
@@ -45,6 +51,17 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
         self.__stdout = PrintWriter(self._callbacks.getStdout(), True)       
 
         self.__stdout.println("Privacy Detector Loaded")
+
+        #Loads patterns file
+        f = open("./patterns.json", "r")
+        patternFile = json.load(f)
+        f.close()
+
+        #precompile regex patterns for better performance
+        self.__regexs = dict()
+        for pattern in patternFile['patterns']:
+            if pattern['use'] == True:
+                self.__regexs[(re.compile(pattern['expression']))] = pattern['type']
         
         # create the log and a lock on which to synchronize when adding log entries
         self._log = ArrayList()
@@ -124,7 +141,7 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
                 # if only path starts with '/api/'
                 if Path.lower().startswith("/api/"):
 
-                    self.__stdout.println(Path)
+                    #self.__stdout.println(Path)
 
                     # Get Response and analyze it
                     httpProxyItemResponse = self._helpers.analyzeResponse(messageInfo.getResponse())
@@ -136,13 +153,13 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
                         if mimeType == "":
                             mimeType = httpProxyItemResponse.getInferredMimeType().lower()
 
-                        self.__stdout.println("mimeType = {}".format(mimeType))
+                        #self.__stdout.println("mimeType = {}".format(mimeType))
 
                         #Check content type one of json types
                         if mimeType == 'json':
-
+                    
                             #Get the response body
-                            responseBody = httpProxyItemResponse.toString()
+                            responseBody = self._helpers.bytesToString(messageInfo.getResponse())
 
                             responseLength = ''
                             #Get header length
@@ -154,13 +171,28 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
                             if responseLength == '':
                                 responseLength = len(responseBody)
 
-                            self.__stdout.println("responseLength = {}".format(responseLength))
+                            #self.__stdout.println("responseLength = {}".format(responseLength))
 
                             if responseLength != '':
-                                self._log.add(LogEntry(toolFlag, self._callbacks.saveBuffersToTempFiles(messageInfo), self._helpers.analyzeRequest(messageInfo).getUrl()))
+
+                                #responseheaderLength = len(httpProxyItemResponse.getHeaders().toString())
+                                #if responseLength < responseheaderLength:
+                                if httpProxyItemResponse.getBodyOffset() != 0:
+                                    responseBody = responseBody[httpProxyItemResponse.getBodyOffset():]
+
+                                #self.__stdout.println(responseBody)
+                                #self.__stdout.println("bodyLength = {}".format(responseLength))
+                                #self.__stdout.println("headerLength = {}".format(responseheaderLength))
+
+                                for regex in self.__regexs.keys():
+                                    matchobj = regex.search(responseBody)
+                                    if matchobj != None:
+                                        self.__stdout.println(Path)
+                                        self.__stdout.println("Matched = {}".format(matchobj.group()))
+                                        self._log.add(LogEntry(toolFlag, self._callbacks.saveBuffersToTempFiles(messageInfo), self._helpers.analyzeRequest(messageInfo).getUrl(), matchobj.group(), self.__regexs.get(regex), self._helpers.analyzeRequest(messageInfo).getMethod()))
           
         except Exception as e:
-            self.__stdout(e)
+            self.__stdout.println(e)
     
         self.fireTableRowsInserted(row, row)
         self._lock.release()
@@ -176,21 +208,33 @@ class BurpExtender(IBurpExtender, ITab, IHttpListener, IMessageEditorController,
             return 0
 
     def getColumnCount(self):
-        return 2
+        return 5
 
     def getColumnName(self, columnIndex):
         if columnIndex == 0:
-            return "Source"
+            return "#"
         if columnIndex == 1:
             return "URL"
+        if columnIndex == 2:
+            return "Method"
+        if columnIndex == 3:
+            return "Matched Pattern"
+        if columnIndex == 4:
+            return "PII Type"
         return ""
 
     def getValueAt(self, rowIndex, columnIndex):
         logEntry = self._log.get(rowIndex)
         if columnIndex == 0:
-            return self._callbacks.getToolName(logEntry._tool)
+            return str(rowIndex)
         if columnIndex == 1:
+            return logEntry._method
+        if columnIndex == 2:
             return logEntry._url.toString()
+        if columnIndex == 3:
+            return logEntry._matched
+        if columnIndex == 4:
+            return logEntry._piitype
         return ""
 
     #
@@ -231,7 +275,10 @@ class Table(JTable):
 #
 
 class LogEntry:
-    def __init__(self, tool, requestResponse, url):
+    def __init__(self, tool, requestResponse, url, matched, piitype, method):
         self._tool = tool
         self._requestResponse = requestResponse
         self._url = url
+        self._matched = matched
+        self._piitype = piitype
+        self._method = method
